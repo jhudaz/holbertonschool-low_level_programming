5f8513f4b330c44577d5d3e3abd4d9b79954f717
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-06-20 10:54:31 -0500

    function that prints all the base10 numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int index;
+for (index = 0; index < 10; index++)
+{
+printf("%d",index);
+}
+putchar('\n');
+return (0);
+}
