194678b35f400655870649f2d2c8eac2f47d0d47
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-06-20 16:46:59 -0500

    using ascii for special characters

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,13 +7,17 @@
 int main(void)
 {
 int ch;
+int space;
+int comma;
+space = 32;
+comma = 44;
 for (ch = 48; ch <= 57; ch++)
 {
 putchar(ch);
 if (ch != 57)
 {
-putchar(',');
-putchar(' ');
+putchar(comma);
+putchar(space);
 }
 }
 putchar('\n');
