5898d21c0d3454d0eedb603a6c9023fc9770404a
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-06-20 11:14:39 -0500

    c program that print numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = '1'; ch <= '9'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
