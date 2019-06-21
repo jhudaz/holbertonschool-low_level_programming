a0734365736819fe7765014b56c2371c305ee62b
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-06-20 20:27:27 -0500

    executable file deleted

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 int ch;
-for (ch = '0'; ch <= '9'; ch++)
+for (ch = 48; ch <= 57; ch++)
 {
 putchar(ch);
-if (ch!='9')
+if (ch!=57)
 {
-putchar(',');
-putchar(' ');
+putchar(44);
+putchar(32);
 }
 }
 putchar('\n');
