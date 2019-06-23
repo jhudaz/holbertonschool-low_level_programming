07d107eb1b146b8532c93ba08a9f80bcb2e38c7b
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-06-23 12:47:51 -0500

    error fixed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,12 +13,15 @@ for (i = 48; i <= 57; i++)
 {
 for (j = 48; j <= 57; j++)
 {
-if(j>=i)
+if (j > i)
 {
 putchar(i);
 putchar(j);
+if (i != 57 && j != 57)
+{
 putchar(44);
-putchar(32);  
+putchar(32);
+}
 }
 }
 }
