ddce056bca1fee36af7a2bf9cd7eba20fd8eee41
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-06-25 17:15:21 -0500

    function changed an variable was add

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,16 +2,18 @@
 /**
  * print_last_digit - print the last digit from  a number
  * @n: variable type int
+ *
  * Return: int
  */
 int print_last_digit(int n)
 {
+	int last;
 
 	if (n < 0)
 	{
 		n = n * -1;
 	}
-	n = n % 10;
-	_putchar(n + '0');
+	last = n % 10;
+	_putchar('0' + last);
 	return (n);
 }
