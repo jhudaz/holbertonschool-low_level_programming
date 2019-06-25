e712323ed5577188f83dfbb2b440d2f6148cd68c
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-06-25 07:14:26 -0500

    removing  executable and the c file add instead

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,19 +9,3 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
-/**
- * print_alphabet - print alphabet
- *
- * Return: void
- */
-void print_alphabet(void)
-{
-	int ch = 'a';
-
-	while (ch <= 'z')
-	{
-		_putchar(ch);
-		ch++;
-	}
-	_putchar('\n');
-}
