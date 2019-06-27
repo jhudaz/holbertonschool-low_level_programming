656cdb45feddf132f1ed57e9b959ee295cd1da10
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-06-27 16:53:42 -0500

    function that print a piramid

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_triangle - function that prints a piramid
+ * @size: type int
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int limit = size;
+
+	for (i = 0; i < size; i++)
+	{
+		limit--;
+		for (j = 0; j < size; j++)
+		{
+			if (j < limit)
+			{
+				_putchar(32);
+			}
+			else
+			{
+				_putchar(35);
+			}
+		}
+		_putchar('\n');
+	}
+}
