c60402bca8bc48c712d25a085adef4c1e4a966f6
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-02 13:58:32 -0500

    adding a null value at the end

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -16,9 +16,10 @@ char *_strcpy(char *dest, char *src)
 		count++;
 		i++;
 	}
-	for (i = 0; i < count; i++)
+	for (i = 0; i <= count; i++)
 	{
 		dest[i] = src[i];
 	}
+	dest[i+1] = 0;
 	return (dest);
 }
