fee62e09af228e518bf7e78c5ba03931171f109d
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-04 17:01:59 -0500

    convert characters in lower  case to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * string_toupper -  convert all
+ * @s: char pointer
+ * Return: int
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < i; j++)
+	{
+		if (s[j] >= 97 && s[j] <= 122)
+		{
+			s[j] =  s[j] - 32;
+		}
+	}
+	return (s);
+}
