966be004dfb99a395bdb342c105e9f040a17fb8f
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-04 09:27:55 -0500

    readme and header files created

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,10 @@
+int _putchar(char c);
+char *_strcat(char *dest, char *src); //0
+char *_strncat(char *dest, char *src, int n); //1
+char *_strncpy(char *dest, char *src, int n); //2
+int _strcmp(char *s1, char *s2); //3
+void reverse_array(int *a, int n); //4
+char *string_toupper(char *); //5
+char *cap_string(char *); //6
+char *leet(char *); //7
+char *rot13(char *); //8
