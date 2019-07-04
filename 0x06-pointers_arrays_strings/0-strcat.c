6f829e2f02d7dbd11e579706c151395a788c7bf6
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-04 13:00:43 -0500

    funtion body changed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,18 +8,15 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
-	int count = 0;
-	int begin = 0;
+	int j = 0;
 
 	while (dest[i] != 0)
 	{
 		i++;
 	}
-	count = i;
-	for (i = count; src[begin] != 0; i++)
+	while (src[j] != 0)
 	{
-		dest[i] = src[begin];
-		begin++;
+		dest[i + 1] = src[j];
 	}
 	dest[i + 1] = '\0';
 	return (dest);
