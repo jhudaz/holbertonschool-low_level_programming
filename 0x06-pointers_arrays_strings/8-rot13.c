9c4e7b8f73742656de76e6426bef8f530956e035
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-05 21:22:32 -0500

    removing standard libraries

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * rot13 - Write a function that concatenates two strings.
  * @s: char pointer
@@ -23,6 +22,5 @@ char *rot13(char *s)
 			}
 		}
 	}
-	printf("count is %d\n", i);
 	return (s);
 }
