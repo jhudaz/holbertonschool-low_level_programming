cc96fd7cdcea226c805c911f01dfc7c25c19183a
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-08 13:47:04 -0500

    function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strchr - function that copies memory area.
+ * @s: pointer to change
+ * @c: char to find in the string
+ *
+ * Return: char.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			break;
+		}
+		s++;
+	}
+	return (s);
+}
