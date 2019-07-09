b32a5008585e282d0ea74b6cd8a355eb7f53d42d
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-09 16:23:08 -0500

    new fixes

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,17 +13,11 @@ char *_strstr(char *haystack, char *needle)
 	int i = 0;
 	int aux = 0;
 	int aux2 = 0;
-	int count;
-
-	while (needle[i] != '\0')
-	{
-		i++;
-	}
-	count = i;
+	int j = 0;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		if (haystack[i] == needle[aux])
+		if (haystack[i] == needle[j])
 		{
 			aux = i;
 			while (haystack[aux] == needle[aux2])
@@ -31,14 +25,13 @@ char *_strstr(char *haystack, char *needle)
 				aux++;
 				aux2++;
 			}
-			if (count == aux2 - 1)
-			{
-				printf("entro!\n");
-				return (haystack + i);
-			}
+			j++;
+		}
+		
+		if (needle[aux2] == '\0')
+		{
+			return (haystack + i);
 		}
 	}
-	printf("count: %d\n", count);
-	printf("aux: %d\n", aux2);
 	return (0);
 }
