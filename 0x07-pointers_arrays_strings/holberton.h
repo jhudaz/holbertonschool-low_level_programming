42f34fd91b3185a77cfca7141c022fd1e4fbd437
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-08 22:23:25 -0500

    function that print a chessboard

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -6,3 +6,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void set_string(char **s, char *to);
+void print_chessboard(char (*a)[8]);
