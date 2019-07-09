e2ee2069125b2f7aa05e8014694b8e31b7ed8428
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-09 14:57:12 -0500

    betty fixes

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,35 +10,35 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-    int i = 0;
-    int aux = 0;
-    int aux2 = 0;
-    int count;
+	int i = 0;
+	int aux = 0;
+	int aux2 = 0;
+	int count;
 
-    while (needle[i] != '\0')
-    {
-        i++;
-    }
-    count = i;
+	while (needle[i] != '\0')
+	{
+		i++;
+	}
+	count = i;
 
-    for (i = 0; haystack[i] != '\0'; i++)
-    {
-        if (haystack[i] == needle[aux])
-        {
-            aux = i;
-            while (haystack[aux] == needle[aux2])
-            {
-                aux++;
-                aux2++;
-            }
-            if (count == aux2 -1)
-            {
-                printf("entro!\n");
-                return (haystack + i);
-            }
-        }
-    }
-    printf("count: %d\n", count);
-    printf("aux: %d\n",aux2);
-    return (0);
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[aux])
+		{
+			aux = i;
+			while (haystack[aux] == needle[aux2])
+			{
+				aux++;
+				aux2++;
+			}
+			if (count == aux2 - 1)
+			{
+				printf("entro!\n");
+				return (haystack + i);
+			}
+		}
+	}
+	printf("count: %d\n", count);
+	printf("aux: %d\n", aux2);
+	return (0);
 }
