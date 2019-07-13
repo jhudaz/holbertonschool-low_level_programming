2e5d557b5688387740fa3b74df5010727c73e6ed
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-13 07:35:25 -0500

    function that print all the arguments with a new line

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - function that prints all the arguments with a new line
+ * @argc: count of the arguments
+ * @argv: pointers array
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+	return (0);
+}
