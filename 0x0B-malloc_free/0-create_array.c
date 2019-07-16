79177b2904e3651c2f3c457261e62fb5c8c836b8
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-16 11:02:55 -0500

    function that reserve in memory a space for an array using malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array - function that reserve in memory for an array
+ * @size: variable int with the value of the size
+ * @c: character value to add in the array
+ * Return: pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	ptr = (char *)malloc(size * sizeof(char));
+
+	if (size <= 0)
+	{
+		ptr = 0;
+		return (ptr);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			ptr[i] = c;
+		}
+		return (ptr);
+	}
+}
