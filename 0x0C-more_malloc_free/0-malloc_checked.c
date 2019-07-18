0f6dacf3255c2eecf0603aa47198b88a8a718ed3
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-18 10:30:57 -0500

    function that alloc a space in memory and exit with a status code if it fails

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: unsigned int
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,8 @@
+# 0x0B-malloc_free
+
+Project using malloc, calloc, realloc and free to handle the memory 
+
+  - 0-malloc_checked.c
+
+
+
