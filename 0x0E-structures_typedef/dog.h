1864fa74faf4e7d6c61270284835ceb76bb5fc50
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-22 11:58:34 -0500

    typedef change

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,10 +7,13 @@
  *
  * Description: Longer description
  */
-struct dog
+typedef struct dog
 {
-	char *name;
-	float age;
+    char *name;
+    float age;
     char *owner;
-};
+}dog_t;
 int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
