b82ba060725c1e244be39326b58096169845310d
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-22 10:19:12 -0500

    a structure was created for dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,14 @@
+# 0x0E-structures_typedef
+
+Project directory with functions using structures and typedef
+
+  - dog.h
+  - 1-init_dog.c
+  - 2-print_dog.c
+  - 3 (add a new structure)
+  - 4-new_dog.c
+  - 5-free_dog.c
+
+
+
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+
+/**
+ * struct dog - first strut for dog
+ * @name: char pointer
+ * @age: float
+ * @owner: char pointer
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+    char *owner;
+};
+int _putchar(char c);
