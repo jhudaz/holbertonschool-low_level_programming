e2fee11448edd6223ad906883e3d57970fd385b5
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-22 13:43:59 -0500

    function that clean the memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dog - function that free the space in memory of the structure.
+ * @d: char pointer to a structure
+ * Return: void
+*/
+void free_dog(dog_t *d)
+{
+	free(d);
+}
