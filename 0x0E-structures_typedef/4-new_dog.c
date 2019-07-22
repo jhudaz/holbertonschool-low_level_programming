2d421e9d693e92e0c3c0319b729a4bb26744127a
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-22 15:07:45 -0500

    using more functions

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * _strlen - function that count the number of characters in a string
+ * @s: char pointer
+ * Return: int
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * _copychar - function that copy a string to another
+ * @dest: char pointer
+ * @src: char pointer
+ * Return: char
+*/
+char *_copychar(char *dest, char *src)
+{
+	int i = 0;
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/**
  * new_dog - function that creates a new dog.
  * @name: char pointer
  * @age: float
@@ -10,11 +42,37 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *my_dog;
+	dog_t *my_dog;
+	char *cpname;
+	char *cpowner;
+	int namelen;
+	int ownerlen;
+
+	namelen = _strlen(name);
+	ownerlen = _strlen(owner);
 
 	my_dog = malloc(sizeof(struct dog));
 	if (my_dog == NULL)
 		return (NULL);
+
+	cpname = malloc(sizeof(char *) * (namelen + 1));
+	if (cpname == NULL)
+	{
+		free(my_dog);
+		free(cpname);
+		return (NULL);
+	}
+
+	cpowner = malloc(sizeof(char *) * (ownerlen + 1));
+	if (cpowner == NULL)
+	{
+		free(my_dog);
+		free(cpowner);
+		return (NULL);
+	}
+
+	cpname = _copychar(cpname, name);
+	cpowner = _copychar(cpowner, name);
 	my_dog->name = name;
 	my_dog->age = age;
 	my_dog->owner = owner;
