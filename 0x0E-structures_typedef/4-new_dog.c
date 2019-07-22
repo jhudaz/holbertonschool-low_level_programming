3f86a60fc32e581cb3d72c5304aaa30560769747
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-22 13:34:26 -0500

    change name of the pointer by the name in the main file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,13 +10,13 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *new_dog;
+	struct dog *my_dog;
 
-	new_dog = malloc(sizeof(struct dog));
-	if (new_dog == NULL)
+	my_dog = malloc(sizeof(struct dog));
+	if (my_dog == NULL)
 		return (NULL);
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
-	return (new_dog);
+	my_dog->name = name;
+	my_dog->age = age;
+	my_dog->owner = owner;
+	return (my_dog);
 }
