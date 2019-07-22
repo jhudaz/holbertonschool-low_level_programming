44dcd1d6cd15ce209f810153bbd0fccccfc6d9fd
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-22 12:10:46 -0500

    changes in  the dog.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef DOG
+#define DOG
 /**
  * struct dog - first strut for dog
  * @name: char pointer
@@ -10,11 +10,15 @@
  */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
-
+/**
+ * dog_t - typedef for dog
+ */
+typedef struct dog dog_t;
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+#endif
