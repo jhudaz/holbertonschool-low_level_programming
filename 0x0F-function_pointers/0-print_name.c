870f98185025ef8e6bf863037eb07ee001a59ce6
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-24 10:40:38 -0500

    function that print a string

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_name - function that print a string
+ * @name: pointer char
+ * @f: function pointer
+ * Return: void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		return;
+	f(name);
+}
