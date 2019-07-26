a7d114207a7fb42372d651b033f1f8eefbc2dbee
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-26 14:29:28 -0500

    feature: add a validation that print (nil) if the string is null

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,14 +13,21 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i = 0;
 	va_list list;
+	char *str;
 
 	if (n == 0)
 		return;
+
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(list, char *));
+		str = va_arg(list, char *);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
 		if (separator == NULL)
 			continue;
 		else if (i == (n - 1))
