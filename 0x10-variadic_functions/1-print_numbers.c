b13215502bcac11e9fc7e0d105607b8342b86163
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-07-26 12:00:32 -0500

    feature: function that prints a serie of numbers separated by a specific separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * print_numbers - function that print n number of integers
+ * @separator: char separator
+ * @n: int nuber of arguments
+ * @...: elipsis
+ * Return: void
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list list;
+
+	if (n == 0)
+		return;
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list, int));
+		if (separator == NULL)
+			continue;
+		else if (i == (n - 1))
+			continue;
+		else
+			printf("%s", separator);
+	}
+	va_end(list);
+	printf("\n");
+}
