43a2211d9c922e29ad5dfb2843695854d0f4af35
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-08-01 14:23:23 -0500

    feature: function that print string in a linked list and return the number of nodes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - function that print the elements in a linked list
+ * @h: pointer structure
+ * Return: int count of nodes
+*/
+unsigned long int print_list(list_t *h)
+{
+	list_t *current = h;
+	int i = 0;
+
+	while (current != '\0')
+	{
+		if (!current->str)
+			printf("[0](nill)\n");
+		else
+			printf("[%d]%s\n", current->len, current->str);
+		current = current->next;
+		i++;
+	}
+
+	return (i);
+}
