fb06ca87fc98eb4ccff04f8a1f5eca3f304332e6
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-08-01 21:53:40 -0500

    fix: free the malloc

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -29,8 +29,9 @@ list_t *add_node(list_t **head, const char *str)
 	char *cpy = strdup(str);
 
 	new = malloc(sizeof(list_t));
-	if (!new)
+	if (new == NULL)
 	{
+		free(new);
 		return (NULL);
 	}
 	new->str = strdup(str);
