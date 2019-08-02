a15f72a9d1676c5bb531ef21081ee67ce1e738da
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-08-01 22:10:02 -0500

    fix: validation for the str argument

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -28,13 +28,12 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 	char *cpy = strdup(str);
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-	{
-		free(new);
+	if (!str)
 		return (NULL);
-	}
-	new->str = strdup(str);
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	new->str = strdup(cpy);
 	new->len = _strlen(cpy);
 	new->next = *head;
 	*head = new;
