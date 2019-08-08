5766d9f6e806e5ee00295c1661b74f541bb7c5aa
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-08-08 17:42:24 -0500

    fix: changing body function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,16 +2,22 @@
 #include "holberton.h"
 /**
  * _pow_recursion - function that return the power from a number
+ * @x: int base
  * @y: int pow
  * Return: int
  */
-int _pow_recursion(int y)
+int _pow_recursion(int x, int y)
 {
 	if (y == 0)
 	{
 		return (1);
 	}
-	return (2 * _pow_recursion(y - 1));
+	else if (y < 0)
+	{
+		return (-1);
+	}
+
+	return (x * _pow_recursion(x, (y - 1)));
 }
 /**
  * _strlen - return the amount of characters in a string
@@ -47,7 +53,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		res += (b[i] - '0') * _pow_recursion(pw);
+		res += (b[i] - '0') * _pow_recursion(2, pw);
 		pw--;
 		i++;
 	}
