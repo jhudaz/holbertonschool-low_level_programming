3b722e8a4119c9e36e28b6da1a71dc3b0421d75f
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-08-08 17:27:21 -0500

    update: a new validation to check is the string is null

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -43,6 +43,8 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int count = _strlen(b);
 	unsigned int pw = count - 1;
 
+	if (b == NULL)
+		return (0);
 	if (count > 0)
 	{
 		while (b[i] != '\0')
