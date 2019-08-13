3270d63984ca3d7ea4c306bcf236849bcf9303bf
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-08-13 16:54:45 -0500

    feature: function that create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+/**
+ * _strlen - function that count the number of character in a string
+ * @str: char pointer
+ * Return: int
+*/
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * create_file - function that create a file
+ * @filename: char pointer
+ * @text_content: char pointer
+ * Return: int
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int wr;
+	int length;
+
+	length = _strlen(text_content);
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0700);
+	if (fd == -1)
+		return (-1);
+
+	wr = write(fd, text_content, length);
+
+	if (wr == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
