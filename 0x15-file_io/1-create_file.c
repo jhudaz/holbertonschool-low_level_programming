5f2a54da07ae44beed257409be5674b4d25aaf48
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-08-13 16:59:14 -0500

    fix: validation for text content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -39,7 +39,11 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0700);
 	if (fd == -1)
 		return (-1);
-
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (-1);
+	}
 	wr = write(fd, text_content, length);
 
 	if (wr == -1)
