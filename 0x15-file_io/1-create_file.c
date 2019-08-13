70a5b5ac4090c7caacb80986513d1e9c1c98b214
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-08-13 17:08:20 -0500

    fix: change flag

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -42,12 +42,12 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		close(fd);
-		return (-1);
+		return (1);
 	}
 	wr = write(fd, text_content, length);
-
+	close(fd);
 	if (wr == -1)
 		return (-1);
-	close(fd);
+
 	return (1);
 }
