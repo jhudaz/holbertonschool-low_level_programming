7d35f8147e5ffebf022415e2f88ea9a14f18163c
jandres <jandres.vr@hotmail.com>
2019-10-11 16:28:40 -0500

    feature: function that retrieve a value from the hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get a value froma hash table.
+ * @ht: hash_table_t pointer
+ * @key: char pointer
+ * Return: Always EXIT_SUCCESS.
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+
+	idx = key_index((unsigned char *)key, ht->size);
+
+	while (ht->array[idx])
+	{
+		if (strcmp((ht->array[idx])->key, key) == 0)
+		{
+			return ((ht->array[idx])->value);
+		}
+		ht->array[idx] = (ht->array[idx])->next;
+	}
+	return (NULL);
+}
