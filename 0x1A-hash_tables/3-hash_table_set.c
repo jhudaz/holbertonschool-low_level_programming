2e4f24a88c59e706f5561c7215761f0f49c0f62d
jandres <jandres.vr@hotmail.com>
2019-10-11 14:21:28 -0500

    feature: function that add an element at the beginning of a linked list

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function add elements to the hash table
+ * @ht: hash_table_t pointer
+ * @key: char pointer
+ * @value: char pointer
+ * Return: 1 success - 0 failure
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx;
+	hash_node_t *node;
+
+	if (!ht || !key || !value)
+		return (0);
+
+	idx = key_index((unsigned char *)key, ht->size);
+
+	node = malloc(sizeof(hash_node_t *));
+	if (!node)
+		return (0);
+
+	node->value = strdup(value);
+	node->key = strdup(key);
+	node->next = NULL;
+
+	if (ht->array[idx] == NULL)
+	{
+		ht->array[idx] = node;
+	}
+	else
+	{
+		node->next = ht->array[idx];
+		ht->array[idx] = node;
+	}
+	return (1);
+}
