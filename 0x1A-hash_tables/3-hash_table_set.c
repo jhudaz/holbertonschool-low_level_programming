ac2e25cca5a728cd1dd2aaf86dfc516eab504b1a
jandres <jandres.vr@hotmail.com>
2019-10-11 17:18:20 -0500

    fix: lenght of  key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,6 +16,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx;
 	hash_node_t *node;
 
+	if (strlen(key) == 0)
+		return (0);
+
 	if (!ht || !key || !value)
 		return (0);
 
