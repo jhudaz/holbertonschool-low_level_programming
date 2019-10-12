30d5798baec339071a6b6af3c456287eb69265f4
jandres <jandres.vr@hotmail.com>
2019-10-12 00:35:31 -0500

    feature: more free

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,5 +10,23 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int idx;
+	hash_node_t *current;
+
+	if (!ht)
+		exit(1);
+
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		current = ht->array[idx];
+		while (ht->array[idx])
+		{
+			current = ht->array[idx];
+			ht->array[idx] = ht->array[idx]->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+		}
+	}
 	free(ht);
 }
