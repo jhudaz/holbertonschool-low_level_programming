1aebcd70cf73c9f457e3b563fcdecbceb87abac0
jandres <jandres.vr@hotmail.com>
2019-10-30 15:12:09 -0500

    feature: changes in the function

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -17,7 +17,7 @@ void bubble_sort(int *array, size_t size)
 	while (state == 1)
 	{
 		state = 0;
-		for (i = 0; i < size; i++)
+		for (i = 0; i < size - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
