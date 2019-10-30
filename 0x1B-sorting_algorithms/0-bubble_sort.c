1474a09f14cafecf0cfaeff8d0e65d053c175b4a
jandres <jandres.vr@hotmail.com>
2019-10-30 14:50:17 -0500

    feature: bubble sort

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * bubble_sort - sort an array using the bubble method
+ * @array: int pointer
+ * @size: array size
+ * Return: void
+*/
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i;
+	unsigned int tmp;
+	unsigned int state;
+
+	state = 1;
+
+	while (state)
+	{
+		state = 0;
+		for (i = 0; i < size; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+				state = 1;
+			}
+		}
+	}
+}
