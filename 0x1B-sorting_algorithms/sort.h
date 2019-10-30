167616669b792850467f54d18e60f419c58725f4
jandres <jandres.vr@hotmail.com>
2019-10-30 11:22:14 -0500

    fix: betty changes in the library

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -9,11 +9,14 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void swap(listint_t *nodeL, listint_t *nodeR);
 #endif
