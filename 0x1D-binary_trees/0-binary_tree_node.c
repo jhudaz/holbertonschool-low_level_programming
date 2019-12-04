45ef6aed845f0c570ba51f7129d1cd191841cd63
jandres <jandres.vr@hotmail.com>
2019-12-04 12:09:55 -0500

    fix: changes in task 0

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *tmp;
 
 	tmp = malloc(sizeof(binary_tree_t));
-	if s(!tmp)
+	if (!tmp)
 		return (NULL);
 
 	if (!parent)
