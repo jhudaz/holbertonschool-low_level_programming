6ced0f42062c07cba917eb5fe7a2821a0e629683
jaime <jandres.vr@hotmail.com>
2020-01-15 11:37:16 -0500

    feature: header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,6 @@
+#ifndef SEARCH_ALGORITHMS
+#define SEARCH_ALGORITHMS
+#include <stdio.h>
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
